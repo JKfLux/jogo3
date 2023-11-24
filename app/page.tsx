993b5c2e5031c49675c2b5266840ff95c1ffd084
João Koch <147113779+JKfLux@users.noreply.github.com>
2023-11-24 16:01:56 -0300

    Jogo3

--- app/page.tsx
+++ app/page.tsx
@@ -3,41 +3,45 @@
 import Image from 'next/image'
 
 export default function Home() {
-
+  let table = ["", "", "", "", "", "", "", "", ""]
   let jogador = "X"
 
   function marcar(td: any) {
     if (td.innerHTML == "") {
       td.innerHTML = jogador
     }
+    else {
+      alert("Jogar Novamente!")
+    }
     if (jogador == "X") {
       jogador = "O"
 
     } else {
       jogador = "X"
-    }
-  }
 
+    }
+
+  }
   return (
     <section>
-      <header>Jogo da Velha</header>
-      <main>
-        <table>
+      <header className='flex justify-center text-center mt-11 text-8xl font-black font-sans' >Jogo da Velha</header>
+      <main className='text-center font-extrabold text-4xl'>
+        <table className='flex justify-center text-center mt-11'>
           <tbody>
             <tr>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
             </tr>
             <tr>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4 border-black w-[100px] h-[100px] hover:bg-white'></td>
             </tr>
             <tr>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
-              <td onClick={(td)=> marcar(td.target)} className='border-4 border-red w-[100px] h-[100px]'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4  border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4  border-black w-[100px] h-[100px] hover:bg-white'></td>
+              <td onClick={(td) => marcar(td.target)} className='border-4  border-black w-[100px] h-[100px] hover:bg-white'></td>
             </tr>
           </tbody>
         </table>
